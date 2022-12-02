74f1f85f82e28f036688577e4e7db0d9a753ba2f
tobias-wilfert <s0181439@ad.ua.ac.be>
2022-12-02 17:36:39 +0100

    Update the rules to contain guarded transitions.

--- hidden_code/translational_rules.py
+++ hidden_code/translational_rules.py
@@ -59,3 +59,76 @@ leftID = str(getAttr('pId','0'))
 rightID = str(getAttr('pId','1'))
 
 setAttr('tname', leftID + '_trans_' + rightID, '7')
+
+
+
+# -- Source Rule: Action --
+sourceRate = int(getAttr('rate', '0'))
+sourceID = str(getAttr('pId','0'))
+
+# Set the values related to the tile
+setAttr('capacity',sourceRate, '2')
+setAttr('weight',sourceRate, '7')
+
+# Name the transitions and places related to the tile
+setAttr('tname', sourceID+'spawn','1')
+setAttr('pname', sourceID+'wait','2')
+setAttr('tname', sourceID+'out','4')
+
+# Name the guard related
+setAttr('pname', 'inG' + sourceID, '11')
+setAttr('pname', sourceID + 'g', '12')
+setAttr('pname', sourceID + 'outG', '13')
+
+setAttr('tname', sourceID + 'g1', '18')
+setAttr('tname', sourceID + 'g2', '19')
+
+
+# -- Segement Rule: Action --
+segmentID = str(getAttr('pId','0'))
+
+# Name the place related items
+setAttr('pname', 'in0_' + segmentID + '_out0', '1')
+
+# Name the guard related items
+setAttr('pname', 'inG_' + segmentID + '_outG', '2')
+
+
+# -- Sink Rule: Action --
+segmentID = str(getAttr('pId','0'))
+
+# Name the place related items
+setAttr('pname', 'in0_' + segmentID,'1')
+
+# Name the guard related items
+setAttr('pname', 'inG_' + segmentID + '_outG', '3')
+
+
+# -- Confluence Rule: Action --
+cofluenceID = str(getAttr('pId','0'))
+
+# Name the transitions and places realted to the tile
+setAttr('pname', 'in0_' + cofluenceID, '5')
+setAttr('pname', cofluenceID + 'confluence', '6')
+setAttr('pname', cofluenceID + 'guard0', '7')
+setAttr('pname', cofluenceID + 'guard1', '8')
+setAttr('pname', 'in1_' + cofluenceID, '9')
+setAttr('pname', cofluenceID + '_out0', '10')
+setAttr('pname', cofluenceID + '_out1', '11')
+
+setAttr('tname', cofluenceID + 't_in0', '1')
+setAttr('tname', cofluenceID + 't_in1', '2')
+setAttr('tname', cofluenceID + 't_out0', '3')
+setAttr('tname', cofluenceID + 't_out1', '4')
+
+# Name the transitions and places realted to the guard
+setAttr('pname', 'inG_' + cofluenceID , '28')
+setAttr('pname', cofluenceID + 'g1', '29')
+setAttr('pname', cofluenceID + 'g2', '30')
+setAttr('pname', cofluenceID + 'g3', '31')
+setAttr('pname', cofluenceID + '_outG', '32')
+
+setAttr('tname', cofluenceID + 'g4', '33')
+setAttr('tname', cofluenceID + 'g5', '34')
+setAttr('tname', cofluenceID + 'g6', '35')
+setAttr('tname', cofluenceID + 'g7', '36')
