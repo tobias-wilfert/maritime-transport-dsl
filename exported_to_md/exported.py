4aacdd4ef3ca2e048e21fccfa85e9a0b6becc3d9
tobias-wilfert <s0181439@ad.ua.ac.be>
2022-12-14 09:50:28 +0100

    Added the base for task4

---
+++ exported_to_md/exported.py
@@ -0,0 +1,101 @@
+
+
+PUT YOUR TEMPLATE CODE HERE :)
+
+Here is an example of how to use EGL:
+
+Port info:
+  c2 in-/out-port 0 -> c_c2_LB
+  c2 in-/out-port 1 -> w_c2_LC
+  c2 in-/out-port 2 -> w_CP_c2
+  c4 in-/out-port 0 -> c_LC_c4
+  c4 in-/out-port 1 -> c_c4_d4
+  c4 in-/out-port 2 -> c_c4_d3
+  c4 in-/out-port 3 -> c_c4_d5
+  c4 in-/out-port 4 -> c_c5_d4
+  CP in-/out-port 0 -> w_CP_c2
+  CP in-/out-port 1 -> w_CP_LA
+  CP in-/out-port 2 -> w_c1_CP
+  c1 in-/out-port 0 -> w_c1_S
+  c1 in-/out-port 1 -> w_c1_CP
+  c5 in-/out-port 0 -> c_c5_d4
+  c5 in-/out-port 1 -> c_c5_d2
+  c5 in-/out-port 2 -> c_c3_c5
+  c7 in-/out-port 0 -> c_c7_c6
+  c7 in-/out-port 1 -> c_c7_d6
+  c7 in-/out-port 2 -> c_c7_d7
+  c3 in-/out-port 0 -> c_c3_c5
+  c3 in-/out-port 1 -> c_d1_c3
+  c3 in-/out-port 2 -> c_LA_c3
+  c6 in-/out-port 0 -> c_c7_c6
+  c6 in-/out-port 2 -> c_c6_LB
+  c6 in-/out-port 1 -> c_c6_d8
+
+
+Routing info:
+  c2 -> d1 via port 2
+  c2 -> d8 via port 0
+  c2 -> d2 via port 1
+  c2 -> d7 via port 0
+  c2 -> d5 via port 1
+  c2 -> d4 via port 1
+  c2 -> d3 via port 1
+  c2 -> d6 via port 0
+  c4 -> d1 via port 4
+  c4 -> d8 via port 0
+  c4 -> d2 via port 4
+  c4 -> d7 via port 0
+  c4 -> d5 via port 3
+  c4 -> d4 via port 1
+  c4 -> d3 via port 2
+  c4 -> d6 via port 0
+  CP -> d1 via port 1
+  CP -> d8 via port 0
+  CP -> d2 via port 1
+  CP -> d7 via port 0
+  CP -> d5 via port 0
+  CP -> d4 via port 0
+  CP -> d3 via port 0
+  CP -> d6 via port 0
+  c1 -> d1 via port 1
+  c1 -> d8 via port 1
+  c1 -> d2 via port 1
+  c1 -> d7 via port 1
+  c1 -> d5 via port 1
+  c1 -> d4 via port 1
+  c1 -> d3 via port 1
+  c1 -> d6 via port 1
+  c5 -> d1 via port 2
+  c5 -> d8 via port 0
+  c5 -> d2 via port 1
+  c5 -> d7 via port 0
+  c5 -> d5 via port 0
+  c5 -> d4 via port 0
+  c5 -> d3 via port 0
+  c5 -> d6 via port 0
+  c7 -> d1 via port 0
+  c7 -> d8 via port 0
+  c7 -> d2 via port 0
+  c7 -> d7 via port 2
+  c7 -> d5 via port 0
+  c7 -> d4 via port 0
+  c7 -> d3 via port 0
+  c7 -> d6 via port 1
+  c3 -> d1 via port 1
+  c3 -> d8 via port 2
+  c3 -> d2 via port 0
+  c3 -> d7 via port 2
+  c3 -> d5 via port 0
+  c3 -> d4 via port 0
+  c3 -> d3 via port 0
+  c3 -> d6 via port 2
+  c6 -> d1 via port 2
+  c6 -> d8 via port 1
+  c6 -> d2 via port 2
+  c6 -> d7 via port 0
+  c6 -> d5 via port 2
+  c6 -> d4 via port 2
+  c6 -> d3 via port 2
+  c6 -> d6 via port 0
+
+
